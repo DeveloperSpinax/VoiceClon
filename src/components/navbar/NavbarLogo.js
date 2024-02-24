e9acdf93d34ffa5cf4cf6a0d67cc8d1ab9aff671
DeveloperSpinax <developerspinax@gmail.com>
2024-02-24 20:10:17 +0500

    v5.5

--- src/components/navbar/NavbarLogo.js
+++ src/components/navbar/NavbarLogo.js
@@ -1,7 +1,8 @@
 // React builtin components imports
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 // Custom component React
+import DropDownFlags from '../dropDown/DropDownFlags';
 
 // Material UI
 import AppBar from '@mui/material/AppBar';
@@ -14,12 +15,15 @@ import CssBaseline from '@mui/material/CssBaseline';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Button from '@mui/material/Button';
 import { useTheme } from '@mui/material/styles';
+import Menu from '@mui/material/Menu';
+import { IconButton, useMediaQuery, MenuItem, MenuList } from '@mui/material';
+
 
 // Icons and Images
 import Logo from '../../static/landingPage/Logo_edited.png';
-import DropDownFlags from '../dropDown/DropDownFlags';
-import { useMediaQuery } from '@mui/material';
-
+import MenuIcon from '@mui/icons-material/Menu';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
 
 function ElevationScroll(props) {
   const { children, window } = props;
@@ -39,12 +43,62 @@ ElevationScroll.propTypes = {
   window: PropTypes.func,
 };
 
+const pages = ['Products', 'Pricing', 'Blog'];
+
+const page = [
+  {
+    name: "Use Case",
+    icon: '',
+    subMenu : ["Item 1", "Item 2"]
+  },
+  {
+    name: "Feature",
+    icon: '',
+    subMenu : ["Item 1", "Item 2"]
+  },
+  {
+    name: "Resource",
+    icon: '',
+    subMenu : ["Item 1", "Item 2"]
+  },
+  {
+    name: "Pricing",
+    icon: '',
+    subMenu : "none"
+  },
+
+]
+
 const NavbarLogo = (props) => {
 
   const [renderLang, setRenderLang] = React.useState('ENG');
   const [hasScrolled, setHasScrolled] = React.useState(false);
   const theme = useTheme();
   const isLargeScreen = useMediaQuery(theme.breakpoints.up('lg'));
+  const isMobileScreen = useMediaQuery("(max-width: 900px)")
+  const isTabletScreen = useMediaQuery("(max-width: 1200px)")
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [openSubMenuIndex, setOpenSubMenuIndex] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleSubMenuToggle = (index) => {
+    setOpenSubMenuIndex(prevIndex => (prevIndex === index ? null : index)); // Toggle open submenu index
+  };
 
   React.useEffect(() => {
     const handleScroll = () => {
@@ -84,7 +138,7 @@ const NavbarLogo = (props) => {
                 <Box
                     component="img"
                     sx={{
-                        height: "63px",
+                        height: isTabletScreen ? "50px" : "63px",
                         display: { xs: 'flex', color: '#000' },
                         ml: { sm: 2, md: 3, lg: 10 }
                     }}
@@ -93,7 +147,7 @@ const NavbarLogo = (props) => {
                 />
 
                 <Typography
-                  variant="h4"
+                  variant={isTabletScreen ? "h5" : "h4"}
                   noWrap
                   sx={{
                   ml: 2,
@@ -115,84 +169,158 @@ const NavbarLogo = (props) => {
                 <Box sx={{ marginLeft: 'auto', display: 'flex', alignItems: 'center' }}>
 
                   <Box sx={{
-                    mr: 1
+                    mr: 1,
+                    display: isMobileScreen && "none"
                   }}>
                     <DropDownFlags handleParentLang={handleLangChange} setRenderLang={setRenderLang}  />
                   </Box>
 
-                  {
-                    renderLang === "ENG" && (
-                      <>
-                        <Button sx={{ 
-                          mr: 2.5,
-                          mt: 0.5,
-                          color: '#3b99ed', 
-                          textTransform: 'capitalize',
-                          }}
-                          component={Link}
-                          to="/login"
+                  <Box>
+                    {
+                      renderLang === "ENG" && (
+                        <>
+                          <Button sx={{ 
+                            mr: 2.5,
+                            mt: 0.5,
+                            color: '#3b99ed', 
+                            textTransform: 'capitalize',
+                            }}
+                            component={Link}
+                            to="/login"
+                            >
+                                <Typography variant={isMobileScreen ? "body2" : "h6"} sx={{ fontWeight: 700, fontSize: "18px" }}>Login</Typography>
+                          </Button>
+                          <Button sx={{
+                              boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)', 
+                              color: '#fff', 
+                              mr: { xs: 2, md: 8},
+                              background: 'linear-gradient(20deg, #2786d7, #5df9fd)',
+                              borderRadius: '5px', 
+                              textTransform: 'capitalize', 
+                              '&:hover': { 
+                                  bgcolor: 'linear-gradient(60deg, #33CCFF,#D499FF)',
+                                  boxShadow: '0px 3px 5px -1px rgb(106 207 179 / 82%), 0px 6px 10px 0px rgb(219 148 189 / 77%), 0px 1px 18px 0px rgb(144 214 223)'
+                                  } 
+                              }} 
+                              variant="contained" 
+                              component={Link} 
+                              to="/signup"
+                              size={isMobileScreen ? "large" : "large"}
                           >
-                              <Typography variant="h6" sx={{ fontWeight: 700 }}>Login</Typography>
-                        </Button>
-                        <Button sx={{
-                            boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)', 
-                            color: '#fff', 
-                            mr: { xs: 2, md: 8},
-                            background: 'linear-gradient(20deg, #2786d7, #5df9fd)',
-                            borderRadius: '5px', 
-                            textTransform: 'capitalize', 
-                            '&:hover': { 
-                                bgcolor: 'linear-gradient(60deg, #33CCFF,#D499FF)',
-                                boxShadow: '0px 3px 5px -1px rgb(106 207 179 / 82%), 0px 6px 10px 0px rgb(219 148 189 / 77%), 0px 1px 18px 0px rgb(144 214 223)'
-                                } 
-                            }} 
-                            variant="contained" 
-                            component={Link} 
-                            to="/signup"
-                            size="large"
-                        >
-                            <Typography variant="h6">Sign Up</Typography>
-                        </Button>
-                      </>
-                    )
-                  }
+                              <Typography variant={isMobileScreen ? "body2" : "h6"} sx={{ fontWeight: 700, fontSize: "18px" }}>Sign Up</Typography>
+                          </Button>
+                        </>
+                      )
+                    }
 
-                  {
-                    renderLang === "ESP" && (
-                      <>
-                        <Button sx={{ 
-                          mr: 2.5,
-                          mt: 0.5,
-                          color: '#3b99ed', 
-                          textTransform: 'capitalize',
-                          }}
-                          component={Link}
-                          to="/login-esp"
+                    {
+                      renderLang === "ESP" && (
+                        <>
+                          <Button sx={{ 
+                            mr: 2.5,
+                            mt: 0.5,
+                            color: '#3b99ed', 
+                            textTransform: 'capitalize',
+                            }}
+                            component={Link}
+                            to="/login-esp"
+                            >
+                                <Typography variant={isMobileScreen ? "body2" : "h6"} sx={{ fontWeight: 700, fontSize: "18px" }}>Acceso</Typography>
+                          </Button>
+                          <Button sx={{
+                              boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)', 
+                              color: '#fff', 
+                              mr: { xs: 2, md: 8},
+                              background: 'linear-gradient(20deg, #2786d7, #5df9fd)',
+                              borderRadius: '5px', 
+                              textTransform: 'capitalize', 
+                              '&:hover': { 
+                                  bgcolor: 'linear-gradient(60deg, #33CCFF,#D499FF)',
+                                  boxShadow: '0px 3px 5px -1px rgb(106 207 179 / 82%), 0px 6px 10px 0px rgb(219 148 189 / 77%), 0px 1px 18px 0px rgb(144 214 223)'
+                                  } 
+                              }} 
+                              variant="contained" 
+                              component={Link} 
+                              to="/signup-esp"
+                              size="large"
                           >
-                              <Typography variant="h6" sx={{ fontWeight: 700 }}>Acceso</Typography>
-                        </Button>
-                        <Button sx={{
-                            boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)', 
-                            color: '#fff', 
-                            mr: { xs: 2, md: 8},
-                            background: 'linear-gradient(20deg, #2786d7, #5df9fd)',
-                            borderRadius: '5px', 
-                            textTransform: 'capitalize', 
-                            '&:hover': { 
-                                bgcolor: 'linear-gradient(60deg, #33CCFF,#D499FF)',
-                                boxShadow: '0px 3px 5px -1px rgb(106 207 179 / 82%), 0px 6px 10px 0px rgb(219 148 189 / 77%), 0px 1px 18px 0px rgb(144 214 223)'
-                                } 
-                            }} 
-                            variant="contained" 
-                            component={Link} 
-                            to="/signup-esp"
-                            size="large"
-                        >
-                            <Typography variant="h6">Inscribirse</Typography>
-                        </Button>
-                      </>
-                    )
-                  }
+                              <Typography variant={isMobileScreen ? "body2" : "h6"} sx={{ fontWeight: 700, fontSize: "18px" }}>Inscribirse</Typography>
+                          </Button>
+                        </>
+                      )
+                    }
+                  </Box>
+                </Box>
+
+                <Box sx={{
+                  display: isMobileScreen ? "flex" : "none"
+                }}>
+                  <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
+                  >
+                    <MenuIcon sx={{ color: "#3b99ed" }} />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                      vertical: 'bottom',
+                      horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'left',
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    PaperProps={{
+                      sx: {
+                        width: '100%',
+                        maxWidth: '100%',
+                        backgroundColor: 'white',
+                        ml: 2
+                      },
+                    }}
+                    sx={{
+                      display: { xs: 'block', md: 'none' },
+                    }}
+                  >
+                    {pages.map((page, index) => (
+          <div key={page}>
+            <MenuItem
+              onClick={() => handleSubMenuToggle(index)} // Toggle the submenu on click
+            >
+              <Typography>{page}</Typography>
+              {openSubMenuIndex === index ? <ExpandLess /> : <ExpandMore />}
+              {openSubMenuIndex === index && (
+                <Menu
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  transformOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  open={true}
+                  onClose={() => setOpenSubMenuIndex(null)} // Close submenu on outside click
+                >
+                  <MenuItem onClick={handleCloseNavMenu}>Submenu 1</MenuItem>
+                  <MenuItem onClick={handleCloseNavMenu}>Submenu 2</MenuItem>
+                  {/* Add more submenu items as needed */}
+                </Menu>
+              )}
+            </MenuItem>
+          </div>
+        ))}
+                  </Menu>
+
                 </Box>
     
             </Toolbar>
